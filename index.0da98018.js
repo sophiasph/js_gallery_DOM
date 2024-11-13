a2b6754db2ab179d469e76cb068fe6299089df55
Sophia <sophia13gal@gmail.com>
2024-11-13 16:32:59 +0200

    make build

---
+++ index.0da98018.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest("a");r&&(e.src=r.href)});
+//# sourceMappingURL=index.0da98018.js.map
